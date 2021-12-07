ae7ab642c3862336e9076ed4fe9629575fbb644e
Akash Vashisht <akashhvashisht@gmail.com>
2021-12-07 03:53:07 -0800

    Updated ratelistController

--- src/main/java/com/astroenergy/java/astroenergyApplication/controller/RateListController.java
+++ src/main/java/com/astroenergy/java/astroenergyApplication/controller/RateListController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -71,8 +72,8 @@ public class RateListController {
 		}
 	}
 	
-	@RequestMapping("/getRateListDetail")
-	public ResponseEntity<?> getRateListDetail(@RequestBody int id) {
+	@RequestMapping("/getRateListDetail/{id}")
+	public ResponseEntity<?> getRateListDetail(@PathVariable int id) {
 		try {
 			// Ratelist r = rateListRepo.getById(rateList.getSno());
 			// r.setCountryName(rateList.getCountryName());
