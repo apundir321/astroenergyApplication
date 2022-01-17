dfb177cb631d5c2a4b9f100e217f4b96cb68fa80
Anurag Pundir <anuragpundir621@gmail.com>
2022-01-17 18:52:56 +0530

    adding changes

---
+++ src/main/java/com/astroenergy/java/astroenergyApplication/model/AstroOrder.java
@@ -0,0 +1,70 @@
+package com.astroenergy.java.astroenergyApplication.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AstroOrder {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getRazorpayPaymentId() {
+		return razorpayPaymentId;
+	}
+
+	public void setRazorpayPaymentId(String razorpayPaymentId) {
+		this.razorpayPaymentId = razorpayPaymentId;
+	}
+
+	public String getRazorpayOrderId() {
+		return razorpayOrderId;
+	}
+
+	public void setRazorpayOrderId(String razorpayOrderId) {
+		this.razorpayOrderId = razorpayOrderId;
+	}
+
+	public String getRazorpaySignature() {
+		return razorpaySignature;
+	}
+
+	public void setRazorpaySignature(String razorpaySignature) {
+		this.razorpaySignature = razorpaySignature;
+	}
+
+	@Column(name = "astrouser_id")
+    private String userId;
+ 
+	@Column(name = "pay_paymentid")
+    private String razorpayPaymentId;
+ 
+	@Column(name = "payorder_id")
+    private String razorpayOrderId;
+
+	@Column(name = "pay_signature")
+	private String razorpaySignature;
+	
+	
+
+}
---
+++ src/main/java/com/astroenergy/java/astroenergyApplication/model/OrderResponse.java
@@ -0,0 +1,25 @@
+package com.astroenergy.java.astroenergyApplication.model;
+
+public class OrderResponse {
+	 private String applicationFee;
+	    private String razorpayOrderId;
+	    private String secretKey;
+		public String getApplicationFee() {
+			return applicationFee;
+		}
+		public void setApplicationFee(String applicationFee) {
+			this.applicationFee = applicationFee;
+		}
+		public String getRazorpayOrderId() {
+			return razorpayOrderId;
+		}
+		public void setRazorpayOrderId(String razorpayOrderId) {
+			this.razorpayOrderId = razorpayOrderId;
+		}
+		public String getSecretKey() {
+			return secretKey;
+		}
+		public void setSecretKey(String secretKey) {
+			this.secretKey = secretKey;
+		}
+}
